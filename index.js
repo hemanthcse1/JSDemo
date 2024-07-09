8e17feacfcb837d57401b1296704ac81a5e5b178
hemanthcse1 <hemanthcse1@gmail.com>
2024-07-09 10:27:21 +0530

    variables examples

---
+++ index.js
@@ -0,0 +1,49 @@
+// console.log(`hello js world`);
+// console.log("i like pizza");
+
+// window.alert(`This is an alert`);
+
+// window.alert(`i like pizza`);
+
+// document.getElementById("myHeading").textContent = `hello`;
+// document.getElementById("myParagrap").textContent = `I like pizza!`;
+
+// this is a comment
+ 
+// let age = 25;
+// let price =  10.99;
+// let gpa = 2.1;
+
+// console.log(typeof price)
+// console.log(`You are ${age} years old`);
+// console.log(`The price is $${price}`);
+// console.log(`Your gpa is: ${gpa}`)
+
+// let firstName = "Hemanth";
+// let favoriteFood = "Pizza";
+// let email = "hemanthcse1@gmail.com";
+
+// console.log(typeof firstName);
+// console.log(`Your name is ${firstName}`);
+// console.log(`You like ${favoriteFood}`);
+// console.log(`Your Email is ${email}`);
+
+
+// let online = true;
+// let forSale = true;
+// let isStudent = true;
+
+// console.log(`Bro is online : ${online}`);
+// console.log(`Is this car for sale: ${forSale}`);
+// console.log(`Enrolled: ${isStudent}`);
+
+
+// let fullName = "Hemanth Kumar";
+// let age = 32;
+// let isStudent = false;
+
+// document.getElementById("p1").textContent = `Your name is ${fullName}`;
+// document.getElementById("p2").textContent = `You are ${age} years old`;
+// document.getElementById("p3").textContent = `Enrolled: ${isStudent}`;
+
+
